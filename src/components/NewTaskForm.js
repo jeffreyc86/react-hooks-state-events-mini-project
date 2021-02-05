dd5ceeeffda603bb5f4ceaade94de4e1946b5ae4
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-04 19:32:10 -0500

    Done.

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit}) {
+  const [text, setText] = useState("")
+  const [category, setCategory] = useState("")
+
+  function handleSumbit(e){
+    e.preventDefault()
+
+    const newTask = {text, category}
+    onTaskFormSubmit(newTask)
+
+    setText("")
+    setCategory("")
+  }
+
+  const categories = ["Code", "Food", "Money", "Misc"]
+
+  const catOptions = categories.map(cat => {return <option value={cat}>{cat}</option>})
+
   return (
-    <form className="new-task-form">
-      <label>
+    <form className="new-task-form" onSubmit={handleSumbit}>
+      <label htmlFor="details">
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={(e) => setText(e.target.value)} />
       </label>
-      <label>
+      <label htmlFor="category">
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+          {catOptions}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({tasks, onHandleDelete}) {
+
+
+  function handleDelete(text) {
+    onHandleDelete(text)
+  }
+
+  const taskItems = tasks.map (task => {
+    return <Task key={task.text} task={task} onDeleteTask={handleDelete}/>
+  })
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskItems}
     </div>
   );
 }
