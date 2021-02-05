74bad0d42c5d893b889a1c857d80441e13fe0fac
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-04 19:31:33 -0500

    commit tree 884bcc8e32604c899b923ca0f241975db0f2aabc

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,43 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [category, setCategory] = useState("All")
+  const [tasks, setTasks] = useState(TASKS)
+
+
+  const allTasks = tasks.filter(task => {
+    if (category === "All") {
+      return true
+    } else if (category) {
+      return task.category === category
+    }
+  })
+
+  const onHandleDelete = (text) => {
+    let newTasks = [...tasks]
+    newTasks = newTasks.filter(task =>{return task.text !== text})
+    setTasks(newTasks)
+  }
+
+  const addNewTask = (task) => {
+    const newTasks = [task,...tasks]
+    setTasks(newTasks)
+  }
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+        currentCategory={category}
+        categories={CATEGORIES} 
+        setCategory={setCategory}
+        />
+      <NewTaskForm onTaskFormSubmit={addNewTask}/>
+      <TaskList 
+        tasks={allTasks}
+        onHandleDelete={onHandleDelete}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, setCategory, currentCategory }) {
+
+  const categoryBtns = categories.map(category => {
+    return (
+    <button 
+      key={category}
+      value={category} 
+      onClick={(e)=>setCategory(e.target.value)}
+      className={category === currentCategory ? "selected" : null}
+      >
+      {category}
+      </button>)
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryBtns}
     </div>
   );
 }
